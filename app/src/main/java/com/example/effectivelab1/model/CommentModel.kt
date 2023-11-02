b0327637e12efb24a594abce33aa22d5c33780fd
Kiselev Danil <dabikis2855@gmail.com>
2023-11-02 23:52:22 +0600

    [~] update comment model

--- app/src/main/java/com/example/effectivelab1/model/CommentModel.kt
+++ app/src/main/java/com/example/effectivelab1/model/CommentModel.kt
@@ -1,8 +1,10 @@
 package com.example.effectivelab1.model
 
+import androidx.compose.ui.graphics.painter.Painter
+
 data class CommentModel(
     val name: String,
-    val surName: String,
     val date: String,
-    val text: String
+    val text: String,
+    val image: Painter
 )
