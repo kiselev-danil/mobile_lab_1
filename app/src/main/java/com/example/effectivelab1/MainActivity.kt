fd96a78801ec7c86a28ee203057434deb73bdbd8
Kiselev Danil <dabikis2855@gmail.com>
2023-11-09 19:55:36 +0600

    [~] delete unused imports, rename function

--- app/src/main/java/com/example/effectivelab1/MainActivity.kt
+++ app/src/main/java/com/example/effectivelab1/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.effectivelab1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,26 +13,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.effectivelab1.components.DrawHeader
 import com.example.effectivelab1.components.DrawInstallButton
 import com.example.effectivelab1.components.DrawLogo
@@ -52,11 +35,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             EffectiveLab1Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DrawDotaScreen("Android")
                 }
             }
         }
@@ -64,7 +46,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun DrawDotaScreen(name: String, modifier: Modifier = Modifier) {
     val pageWidth = 375.dp;
     val defaultElementBackgroundColor = Color(0xFF050B18)
     val defaultBackgroundColor = defaultElementBackgroundColor//Color.Magenta
@@ -115,6 +97,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     EffectiveLab1Theme {
-        Greeting("Android")
+        DrawDotaScreen("Android")
     }
 }
